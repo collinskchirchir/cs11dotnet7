3755982ac0a3f3770c559787dc222c7a1a1b3149
Collins Chirchir <collinskchirchir@gmail.com>
2023-05-07 22:49:25 +0300

    type sizes

--- vs4win/Chapter02/Vocabulary/Program.cs
+++ vs4win/Chapter02/Vocabulary/Program.cs
@@ -1,3 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-//#error version
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range { int.MinValue:N0} to {int.MaxValue:N0}.");
+Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range { double.MinValue:N0} to {double.MaxValue:N0}.");
+Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range { decimal.MinValue:N0} to {decimal.MaxValue:N0}.");
