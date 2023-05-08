851071d6fa1af5cb4df70bb4b8eadb59cec50076
Collins Chirchir <collinskchirchir@gmail.com>
2023-05-08 11:28:08 +0300

    Formatting Strings

---
+++ vs4win/Chapter02/Formatting/Program.cs
@@ -0,0 +1,11 @@
+﻿// Four parameter values can use named arguments.
+//Console.WriteLine(
+//format: "{0} {1} lived in {2}, {3}.",
+//arg0: "Roger", 
+//arg1: "Cevung",
+//arg2: "Stockholm", 
+//arg3: "Sweden");
+// Five or more parameter values cannot use named arguments.
+Console.WriteLine(
+format: "{0} {1} lived in {2}, {3} and worked in the {4} team at {5}.",
+"Roger", "Cevung", "Stockholm", "Sweden", "Education", "Optimizely");
