5012120de62987b2edd082f4607288762cde1160
Collins Chirchir <collinskchirchir@gmail.com>
2023-05-09 21:29:00 +0300

    Simplyfying usage of the console

--- vs4win/Chapter02/Formatting/Program.cs
+++ vs4win/Chapter02/Formatting/Program.cs
@@ -1,5 +1,5 @@
-﻿Console.Write("Type your first name and press ENTER: ");
-string firstName = Console.ReadLine();
-Console.Write("Type your age and press ENTER: ");
-string age = Console.ReadLine();
-Console.WriteLine($"Hello {firstName}, you look good for {age}");
+﻿Write("Type your first name and press ENTER: ");
+string firstName = ReadLine();
+Write("Type your age and press ENTER: ");
+string age = ReadLine();
+WriteLine($"Hello {firstName}, you look good for {age}");
