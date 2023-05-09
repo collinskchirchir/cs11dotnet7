b85e636a11bf44a2d963724aa5635f0feda95566
Collins Chirchir <collinskchirchir@gmail.com>
2023-05-09 21:35:38 +0300

    getting key input from the user

--- vs4win/Chapter02/Formatting/Program.cs
+++ vs4win/Chapter02/Formatting/Program.cs
@@ -1,5 +1,14 @@
-﻿Write("Type your first name and press ENTER: ");
-string firstName = ReadLine();
-Write("Type your age and press ENTER: ");
-string age = ReadLine();
-WriteLine($"Hello {firstName}, you look good for {age}");
+﻿//Write("Type your first name and press ENTER: ");
+//string firstName = ReadLine();
+//Write("Type your age and press ENTER: ");
+//string age = ReadLine();
+//WriteLine($"Hello {firstName}, you look good for {age}");
+
+// Getting key input from the user using ReadKey method
+Write("Press any key combination");
+ConsoleKeyInfo key = Console.ReadKey();
+WriteLine();
+WriteLine("Key: {0}, Char: {1}, Modifiers: {2}",
+    key.Key,
+    key.KeyChar,
+    key.Modifiers);
