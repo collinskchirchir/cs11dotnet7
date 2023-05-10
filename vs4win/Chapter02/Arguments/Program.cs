38a23d3f5760ebc7f94106b1f87e68ec580e18b4
Collins Chirchir <collinskchirchir@gmail.com>
2023-05-10 12:11:35 +0300

    passing args to a console app

---
+++ vs4win/Chapter02/Arguments/Program.cs
@@ -0,0 +1,24 @@
+﻿WriteLine($"There are {args.Length} arguments.");
+foreach (string arg in args)
+{
+    WriteLine(arg );
+}
+
+if (args.Length < 3)
+{
+    WriteLine("You must specify two colors and cursor size, e.g.");
+    WriteLine("dotnet run red yellow 50");
+    return;
+}
+
+ForegroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof( ConsoleColor ),
+    value: args[0],
+    ignoreCase: true);
+
+BackgroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof( ConsoleColor ),
+    value: args[1],
+    ignoreCase: true);
+
+CursorSize = int.Parse(args[2]);
