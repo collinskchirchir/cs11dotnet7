103941d0ce6a99834786fab5ab8ce09c09441694
Collins Chirchir <collinskchirchir@gmail.com>
2023-05-23 21:35:30 +0300

    explicit casting

---
+++ vs4win/Chapter03/CastingConverting/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 10;
+double b = a; // and int can be safely cast into a double
+WriteLine(b);
+
+double c = 9.8;
+int d = (int)c; 
+WriteLine(d);
+
+long e = 10;
+int f = (int)e;
+WriteLine($"e is {e:N0} and f is {f:N0}");
+
+
+e = 5_000_000_000;
+f = (int)e;
+WriteLine($"e is {e:N0} and f is {f:N0}");
