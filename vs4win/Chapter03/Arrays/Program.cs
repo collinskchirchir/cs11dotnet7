47e8f8fd75dfb65e42321acd72f8ef92563eebe8
Collins Chirchir <collinskchirchir@gmail.com>
2023-05-23 20:59:24 +0300

    arrays

---
+++ vs4win/Chapter03/Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] names; // can reference any size array of strings
+
+// allocating memory for 4 strings in an array
+names = new string[4];
+
+// storing items at index positions
+
+names[0] = "Kate";
+names[1] = "Jack";
+names[2] = "Rebecca";
+names[3] = "Tom";
+
+
+// alt to defining arrays
+
+string[] names2 = new[] { "Kate", "Jack", "Rebbeca2", "Tom2" };
+
+// looping through the names
+for (int i = 0; i < names2.Length; i++)
+{
+    // output the item at index position 1
+    WriteLine(names2[i]);
+}
