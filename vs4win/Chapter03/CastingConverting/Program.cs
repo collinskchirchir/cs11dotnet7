c7f514b46333579221ebec02424e092e38a70718
Collins Chirchir <collinskchirchir@gmail.com>
2023-05-26 21:45:24 +0300

    Handling Exceptions

--- vs4win/Chapter03/CastingConverting/Program.cs
+++ vs4win/Chapter03/CastingConverting/Program.cs
@@ -1,4 +1,6 @@
-﻿int a = 10;
+﻿using static System.Convert;
+
+int a = 10;
 double b = a; // and int can be safely cast into a double
 WriteLine(b);
 
@@ -14,3 +16,8 @@ WriteLine($"e is {e:N0} and f is {f:N0}");
 e = 5_000_000_000;
 f = (int)e;
 WriteLine($"e is {e:N0} and f is {f:N0}");
+
+// Using System.Convert
+double g = 11.5;
+int h = ToInt32(g); // a method of System.Convert
+WriteLine($"g is {g} and h is {h}");
---
+++ vs4win/Chapter03/HandlingExceptions/Program.cs
@@ -0,0 +1,26 @@
+﻿WriteLine("Before Parsing");
+Write("What is your age? ");
+string? input = ReadLine();
+if (string.IsNullOrEmpty(input)) return; // null-handling
+try
+{
+    int age = int.Parse(input!); // the ! is a null-forgiving operator and disables compiler warning.
+    WriteLine($"You are {age} years old.");
+}
+catch (FormatException) when (input.Contains("$")) // catching with filters
+{
+    WriteLine("The age you entered CANNOT contain a dollar symbol!");
+}
+catch (FormatException)
+{
+    WriteLine("The age you entered is not a valid number format.");
+}
+catch (OverflowException)
+{
+    WriteLine("Your age is a valid number format but it is either too biig or small");
+}
+catch (Exception ex)
+{
+    WriteLine($"{ex.GetType()} says {ex.Message}");
+}
+WriteLine("After Parsing");
