b5bbb924b82f680fee3c3014d2207c5ee8457880
Collins Chirchir <collinskchirchir@gmail.com>
2023-05-31 16:24:22 +0300

    documenting functions with XML comments

--- vs4win/Chapter04/WritingFunctions/Program.Functions.cs
+++ vs4win/Chapter04/WritingFunctions/Program.Functions.cs
@@ -57,6 +57,15 @@
 		return amount * rate;
 	}
 
+	/// <summary>
+	/// Pass a 32-bit integer and it will be converted into its ordinal equivalent
+	/// </summary>
+	/// <param name="number">
+	/// Number as a cardinal value e.g. 1, 2, 3, and so on.
+	/// </param>
+	/// <returns>
+	/// Number as ordinal value e.g. 1st, 2nd, 3rd, and so on.
+	/// </returns>
 	static string CardinalToOrdinal(int number)
 	{
 		int lastTwoDigits = number % 100;
