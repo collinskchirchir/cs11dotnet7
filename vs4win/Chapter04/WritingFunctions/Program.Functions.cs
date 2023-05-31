9fbd33f9be537188fcec754486e2b613f0ea268e
Collins Chirchir <collinskchirchir@gmail.com>
2023-05-31 11:30:44 +0300

    TimeTable function

---
+++ vs4win/Chapter04/WritingFunctions/Program.Functions.cs
@@ -0,0 +1,12 @@
+﻿partial class Program
+{
+	static void TimesTable(byte number, byte size = 12)
+	{
+		WriteLine($"This is the {number} times table with {size} rows:");
+		for (int row = 1; row <= size; row++)
+		{
+			WriteLine($"{row} x {number} = {row * number}");
+		}
+		WriteLine();
+	}
+}
---
+++ vs4win/Chapter04/WritingFunctions/Program.cs
@@ -0,0 +1,1 @@
+﻿TimesTable(7);
