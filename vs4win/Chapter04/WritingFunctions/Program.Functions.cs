25867ab6fb601f99bde000a7d51e48a4f866662b
Collins Chirchir <collinskchirchir@gmail.com>
2023-05-31 12:27:21 +0300

    Functions with return value CalculateTax

--- vs4win/Chapter04/WritingFunctions/Program.Functions.cs
+++ vs4win/Chapter04/WritingFunctions/Program.Functions.cs
@@ -9,4 +9,51 @@
 		}
 		WriteLine();
 	}
+
+	static decimal CalculateTax(decimal amount, string twoLetterRegionCode)
+	{
+		decimal rate = 0;
+		switch (twoLetterRegionCode)
+		{
+			case "CH": // Switzerland
+				rate = 0.08M;
+				break;
+
+			case "DK": // Denmark
+			case "NO": // Norway
+				rate = 0.25M;
+				break;
+
+			case "GB": // United Kingdom
+			case "FR": // France
+				rate = 0.2M;
+				break;
+
+			case "HU": // Hungray
+				rate = 0.27M;
+				break;
+
+			case "OR": // Oregon
+			case "AK": // Alaska
+			case "MT": // Montana
+				rate = 0.0M;
+				break;
+
+			case "NK": // North Dakota
+			case "WI": // Wisconsin
+			case "ME": // Maine
+			case "VA": // Virginia
+				rate = 0.05M;
+				break;
+
+			case "CA": // California
+				rate = 0.0825M;
+				break;
+
+			default: // most US States
+				rate = 0.06M;
+				break;
+		}
+		return amount * rate;
+	}
 }
--- vs4win/Chapter04/WritingFunctions/Program.cs
+++ vs4win/Chapter04/WritingFunctions/Program.cs
@@ -1,1 +1,4 @@
-﻿TimesTable(7);
+﻿//TimesTable(7);
+
+decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+WriteLine($"You must pay {taxToPay:C} in tax.");
