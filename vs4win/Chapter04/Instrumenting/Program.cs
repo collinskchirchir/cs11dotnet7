892e79635ffaeb4245892fa50b7fa0c5e21dd61e
Collins Chirchir <collinskchirchir@gmail.com>
2023-06-14 13:35:19 +0300

    unit testing

---
+++ vs4win/Chapter04/Instrumenting/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+
+string logPath = Path.Combine(Environment.GetFolderPath(
+    Environment.SpecialFolder.DesktopDirectory), "log.txt");
+
+WriteLine($"Writing to: {logPath}");
+TextWriterTraceListener logFile = new(File.CreateText(logPath));
+
+Trace.Listeners.Add(logFile);
+
+// text writer is buffered, so this option calls
+// Flush() on all listeners after writing
+Trace.AutoFlush = true;
+
+
+
+Debug.WriteLine("Debug says, I am watching!");
+Trace.WriteLine("Trace says, I am Watching!");
+
+WriteLine("Reading from appsettings.json in {0}",
+    Directory.GetCurrentDirectory());
+ConfigurationBuilder builder = new();
+builder.SetBasePath(Directory.GetCurrentDirectory());
+builder.AddJsonFile("appsettings.json",
+    optional: true, reloadOnChange:true);
+
+IConfigurationRoot configuration = builder.Build();
+
+TraceSwitch ts = new(
+    displayName: "PacktSwitch",
+    description: "This switch is set via a JSON config.");
+
+configuration.GetSection("PacktSwitch").Bind(ts);
+
+Trace.WriteLineIf(ts.TraceError, "Trace error");
+Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+Trace.WriteLineIf(ts.TraceInfo, "Trace information");
+Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
+
+Console.ReadLine();
