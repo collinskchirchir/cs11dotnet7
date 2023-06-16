4713e3f7922892204a24cd2cc6ec22a8c096e6f6
Collins Chirchir <collinskchirchir@gmail.com>
2023-06-16 20:01:38 +0300

    tuples

--- vs4win/Chapter05/PacktLibraryNetStandard2/Person.cs
+++ vs4win/Chapter05/PacktLibraryNetStandard2/Person.cs
@@ -10,5 +10,44 @@ public class Person : object
     public string? Name;
     public DateTime DateOfBirth;
     public WondersOfTheAncientWorld FavoriteAncientWonder;
+    public List<Person> Children = new();
+    public const string Species = "Homo Sapiens"; // constant field
+    public readonly string HomePlanet = "Earth"; // read-only field
+    public readonly DateTime Instantiated;
+
+    // constructors
+    public Person()
+    {
+        Name = "Unknown";
+        Instantiated = DateTime.Now;
+    }
+    public Person(string initialName, string homePlanet)
+    {
+        Name = initialName;
+        HomePlanet = homePlanet;
+        Instantiated = DateTime.Now;
+    }
+
+    // methods
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on  a {DateOfBirth: dddd}.");
+    }
+
+    public string GetOrigin()
+    {
+        return $"{Name} was born on {HomePlanet}.";
+    }
+
+    // tuples
+    public (string, int) GetFruit()
+    {
+        return ("Apples", 5);
+    }
+    // naming fields of a tuple
+    public (string Name, int Number) GetNamedFruit()
+    {
+        return (Name: "Apples", Number: 5);
+    }
 
 }
--- vs4win/Chapter05/PeopleApp/Program.cs
+++ vs4win/Chapter05/PeopleApp/Program.cs
@@ -5,6 +5,8 @@ Person bob = new();
 bob.Name = "Bob Smith";
 bob.DateOfBirth = new DateTime(1965, 12, 22); // C#1.0 or later
 bob.FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
+bob.Children.Add(new() { Name = "Alfred" }); // C# 9.0 and later
+bob.Children.Add(new Person { Name="Zoe" }); // C# 3.0 and later
 
 WriteLine("{0} was born on {1: dddd, d MMMM yyyy} and favorite wonder is {2}",
     bob.Name, bob.DateOfBirth, bob.FavoriteAncientWonder);
@@ -16,3 +18,33 @@ Person alice = new()
 };
 WriteLine("{0} was born on {1: dd MMMM yyyy}",
     alice.Name, alice.DateOfBirth);
+
+WriteLine($"{bob.Name} has {bob.Children.Count} children");
+for (int childIndex = 0; childIndex < bob.Children.Count; childIndex++)
+{
+    WriteLine($"> {bob.Children[childIndex].Name}");
+}
+
+// Making a Field Static
+BankAccount.InterestRate = 0.012M; // strore a shared value
+BankAccount jonesAccount = new();
+jonesAccount.AccountName = "Mrs. Jones";
+jonesAccount.Balance = 2400;
+WriteLine("{0} earned {1:C} interest",
+    jonesAccount.AccountName, jonesAccount.Balance * BankAccount.InterestRate);
+
+Person blankPerson = new(initialName: "Garry", homePlanet: "Mars");
+WriteLine("{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}",
+    blankPerson.Name, blankPerson.HomePlanet, blankPerson.Instantiated);
+
+// methods
+bob.WriteToConsole();
+WriteLine(bob.GetOrigin());
+
+// tuples
+(string, int) fruits = bob.GetFruit();
+WriteLine($"{fruits.Item1}, {fruits.Item2} there are.");
+
+// named fields of a tuples
+var fruitNamed = bob.GetNamedFruit();
+WriteLine($"There are {fruitNamed.Number} {fruitNamed.Name}.");
